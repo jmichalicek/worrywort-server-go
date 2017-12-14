74ba3387f8098c94d8eff59b060f52e69e4396b1
Justin Michalicek <jmichalicek@gmail.com>
2017-12-13 21:24:47 -0500

    user struct receiver test cases

--- worrywort/user_test.go
+++ worrywort/user_test.go
@@ -16,3 +16,57 @@ func TestNewUser(t *testing.T) {
 		t.Errorf("Expected:\n\n%v\n\nGot:\n\n%v", expectedUser, u)
 	}
 }
+
+func TestUserStruct(t *testing.T) {
+	createdAt := time.Now()
+	updatedAt := time.Now().Add(time.Hour * time.Duration(1))
+	u := NewUser(1, "user@example.com", "Justin", "Michalicek", createdAt, updatedAt)
+
+	t.Run("ID()", func(t *testing.T) {
+		var actual int64 = u.ID()
+		expected := u.user.ID
+		if actual != expected {
+			t.Errorf("Expected: %v, got: %v", expected, actual)
+		}
+	})
+
+	t.Run("LastName()", func(t *testing.T) {
+		var actual string = u.LastName()
+		expected := u.user.LastName
+		if actual != expected {
+			t.Errorf("Expected: %v, got: %v", expected, actual)
+		}
+	})
+
+	t.Run("FirstName()", func(t *testing.T) {
+		var actual string = u.FirstName()
+		expected := u.user.FirstName
+		if actual != expected {
+			t.Errorf("Expected: %v, got: %v", expected, actual)
+		}
+	})
+
+	t.Run("Email()", func(t *testing.T) {
+		var actual string = u.Email()
+		expected := u.user.Email
+		if actual != expected {
+			t.Errorf("Expected: %v, got: %v", expected, actual)
+		}
+	})
+
+	t.Run("CreatedAt()", func(t *testing.T) {
+		var actual time.Time = u.CreatedAt()
+		expected := u.user.CreatedAt
+		if actual != expected {
+			t.Errorf("Expected: %v, got: %v", expected, actual)
+		}
+	})
+
+	t.Run("UpdatedAt()", func(t *testing.T) {
+		var actual time.Time = u.UpdatedAt()
+		expected := u.user.UpdatedAt
+		if actual != expected {
+			t.Errorf("Expected: %v, got: %v", expected, actual)
+		}
+	})
+}
