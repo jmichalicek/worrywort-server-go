37d3d430bcdaa1ff2c40671f3a766489e417b0de
Justin Michalicek <jmichalicek@gmail.com>
2017-12-28 15:36:21 -0500

    Started adding db dependencies
    * Fixed Dockerfile.dev to properly install golang/dep
    * Added github.com/jmoiron/sqlx and github.com/DATA-DOG/go-sqlmock to Gopkg files
    * More gofmt which I seem to keep forgetting to run

--- worrywort/user.go
+++ worrywort/user.go
@@ -1,14 +1,15 @@
 package worrywort
 
 import "time"
+
 // Models and functions for user management
 
 type user struct {
 	// really could use email as the pk for the db, but fudging it because I've been trained by ORMs
-	ID int64
+	ID        int64
 	FirstName string
-	LastName string
-	Email string
+	LastName  string
+	Email     string
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
@@ -24,7 +25,7 @@ func NewUser(id int64, email, firstName, lastName string, createdAt, updatedAt t
 		UpdatedAt: updatedAt}}
 }
 
-func (u User) ID() int64 { return u.user.ID }
+func (u User) ID() int64            { return u.user.ID }
 func (u User) FirstName() string    { return u.user.FirstName }
 func (u User) LastName() string     { return u.user.LastName }
 func (u User) Email() string        { return u.user.Email }
