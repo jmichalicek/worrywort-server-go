5bca61feaa38042d38b9744082937bd85e4262a5
Justin Michalicek <jmichalicek@gmail.com>
2018-02-22 03:13:47 +0000

    start changing back to keep password hash on model, just save all fields when saving rather than trying to track if password has changed or not and special case it

--- worrywort/user.go
+++ worrywort/user.go
@@ -26,8 +26,6 @@ type user struct {
 
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
-
-	passwordChanged bool
 }
 
 type User struct {
@@ -61,13 +59,6 @@ func SetUserPassword(u User, password string, hashCost int) (User, error) {
 		return u, err
 	}
 	u.user.Password = string(passwdBytes)
-
-	// For now password hash is not being passed around because it would make NewUser() irritating
-	// so this lets us know password has changed so taht it can be included in save.
-	// But that may change because this kind of sucks, too.  It would be easier to just include the password hash
-	// always when saving... could always use NewUser() to not have password for a truly new user and use other methods
-	// for looking up actual existing users... leaning towards that.
-	u.user.passwordChanged = true
 	return u, nil
 }
 
@@ -75,23 +66,21 @@ func SetUserPassword(u User, password string, hashCost int) (User, error) {
 func (u User) Save(db *sqlx.DB) error {
 	var query string
 	var createdAt time.Time
+	var err error = nil
 	if u.ID() != 0 {
 		query = `UPDATE users SET email = ?, first_name = ?, last_name = ?, password = ?, updated_at = ? WHERE id = ?)`
 		createdAt = u.CreatedAt()
+		_, err := db.Exec(query, u.Email(), u.FirstName(), u.LastName(), u.Password(), createdAt, time.Now(), u.ID())
 	} else {
 		query = `INSERT INTO users (email, first_name, last_name, password, created_at, updated_at)`
 		createdAt = time.Now()
+		_, err := db.Exec(query, u.Email(), u.FirstName(), u.LastName(), u.Password(), createdAt, time.Now())
 	}
-	// db.MustExec(setPass, string(passwdBytes))
-	_, err := db.Exec(query, u.Email, u.FirstName(), u.LastName(), u.Password(), createdAt, time.Now())
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 }
 
-// TODO: Needs to accept set of changes somehow
-func UpdateUser(user User, ) {
+// TODO: Needs to accept set of changes somehow... I think a map of string[interface{}]
+func UpdateUser(user User) {
 
 }
 
@@ -102,13 +91,13 @@ func LookupUser(id int64, db *sqlx.DB) (User, error) {
 	// By default sqlx just passes whatever you type and you need to manually use the correct token...
 	// ? for mysql, $1..$N for postgres, etc.
 	// db.Rebind() will update the string to use the correct bind.
-	query := db.Rebind("SELECT id, first_name, last_name, email, password, created_at, updated_at FROM users WHERE id=?")
+	query := db.Rebind("SELECT id, first_name, last_name, email, password, created_at, updated_at, password FROM users WHERE id=?")
 	err := db.Get(&u, query, id)
 	if err != nil {
 		return User{}, err
 	}
 	// this seems dumb, but it ensures correctness by using the standard NewUser interface
-	return NewUser(u.ID, u.Email, u.FirstName, u.LastName, u.CreatedAt, u.UpdatedAt), nil
+	return u, nil
 }
 
 // Looks up the username (or email, as the case is for now) and verifies that the password
@@ -133,9 +122,7 @@ func AuthenticateLogin(username, password string, db *sqlx.DB) (User, error) {
 		return User{}, pwdErr
 	}
 
-	// Redundant since token.User is already a User{} struct, but this ensures that any other init
-	// which may be needed actually happens.
-	return NewUser(u.ID(), u.Email(), u.FirstName(), u.LastName(), u.CreatedAt(), u.UpdatedAt()), nil
+	return u, nil
 }
 
 // Uses a token as passed in authentication headers by a user to look them up
@@ -155,7 +142,7 @@ func LookupUserByToken(tokenStr string, db *sqlx.DB) (User, error) {
 
 	query := db.Rebind(
 		"SELECT t.token_id, t.token, t.scope, t.expires_at, t.created_at, t.updated_at u.id, u.first_name, u.last_name, " +
-			"u.email, u.created_at, u.updated_at FROM user_authtokens t LEFT JOIN users u ON t.user_id = u.id " +
+			"u.email, u.created_at, u.updated_at, u.password FROM user_authtokens t LEFT JOIN users u ON t.user_id = u.id " +
 			"WHERE t.token_id = ? AND (t.expires_at IS NULL OR t.expires_at < ?)")
 	err := db.Get(&token, query, tokenId, time.Now())
 
@@ -172,9 +159,5 @@ func LookupUserByToken(tokenStr string, db *sqlx.DB) (User, error) {
 		return User{}, pwdErr
 	}
 
-	// Seems a bit silly since token.User is already a User{} struct, but this ensures that any other init
-	// which may be needed actually happens.
-	// I wonder if there's a way to make sqlx do that?
-	return NewUser(token.User.ID(), token.User.Email(), token.User.FirstName(), token.User.LastName(),
-		token.User.CreatedAt(), token.User.UpdatedAt()), nil
+	return u, nil
 }
