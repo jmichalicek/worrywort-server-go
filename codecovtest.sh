97b75a106315e8bd0b39c6c33eb49037c6f04b8f
Justin Michalicek <jmichalicek@gmail.com>
2019-03-04 01:58:35 +0000

    use script to run tests so that codecov logs can be uploaded for multiple packages

---
+++ codecovtest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# script to play nicely with codecov and go test coverprofile
+# from https://github.com/codecov/example-go/blob/master/README.md
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
